1e407aed0248d7ccf16f9dd423ce5014b090c1ae
果丹皮 <wenxingdi@foxmail.com>
2021-10-08 17:25:13 +0800

    printing 1 to 10

---
+++ readme.md
@@ -0,0 +1,3 @@
+This is my readme file for practice assignment 1.
+
+The program print the number 1 to 10.
---
+++ spyder/integers_python/integers.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  8 11:42:07 2021
+
+@author: lenovo
+"""
+
+#%%
+print(1,2,3,4,5,6,7,8,9,10)
+
+#%%
+for i in range (1,11):
+    print(i)
+
+print("that's for all")
